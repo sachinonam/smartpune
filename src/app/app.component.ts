c55c1c144f51f35e7f8dd0ae748d7b2757f2c97c
sachinonam <66748064+sachinonam@users.noreply.github.com>
2020-06-11 20:43:05 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+
 
 
 /* Add Amplify imports */
