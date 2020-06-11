740c4375abcd8ecff5491e624d975bfc937ae0e7
sachinonam <66748064+sachinonam@users.noreply.github.com>
2020-06-11 20:23:49 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+import { withAuthenticator, Connect } from 'aws-amplify-react'
+import Amplify, { graphqlOperation } from 'aws-amplify'
+import { listPostLikes } from '../graphql/queries'
+import awsConfig from './aws-exports.js'
+
+Amplify.configure(awsConfig)
 
 @Component({
   selector: 'app-root',
